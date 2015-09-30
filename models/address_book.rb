0843e2fffa890796140ca0aa38804b0c7a88bf4f
Erwin Nguyen <erwin.nguyen@gmail.com>
2015-09-29 19:17:46 -0700

    CP 24 Search - Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,6 +20,25 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  def binary_search(name)
+    lower = 0
+    upper = entries.length - 1
+    while lower <= upper
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+    return nil
+  end
+
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -27,6 +46,6 @@ class AddressBook
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
-    
+
   end
 end
